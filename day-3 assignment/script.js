1a3571e49cdc6f0779f19f7961b04b2864273032
amnabeel <88191928+amnabeel@users.noreply.github.com>
2021-08-07 03:37:41 -0700

    Add files via upload
    day-3 assignment submitted

---
+++ day-3 assignment/script.js
@@ -0,0 +1,8 @@
+function doSomething(){
+
+    let name=document.querySelector("#n1").value;
+    let city=document.querySelector("#n2").value;
+    document.querySelector("#blk").innerHTML=`<h2>${name}</h2><h3>${city}</h3>`;
+    
+
+}
